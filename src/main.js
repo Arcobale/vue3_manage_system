f8ed7d3045e1ac7a7e44cca0e9c7b22f41125104
Arcobale <1144209486@qq.com>
2023-04-03 18:50:46 +0800

    代码迁移，axios和ElementPlus全局使用

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import locale from 'element-plus/es/locale/lang/zh-cn' //中文
+import axios from 'axios'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.use(ElementPlus, {locale});
+app.config.globalProperties.$https = axios;
+app.mount('#app');
